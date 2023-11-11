f6b8c674963c01d996330c0285f02cbd8c22a56a
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-11 10:03:08 +0900

    initial commit

---
+++ src/main/java/com/example/A/chime/diary/Application.java
@@ -0,0 +1,13 @@
+package com.example.A.chime.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
