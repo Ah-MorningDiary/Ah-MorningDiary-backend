f3155f970f0b88b1de4a58ece1b1522b4b228a98
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-12 03:37:00 +0900

    feat: 최초접속 확인
    해당날짜에 푼 퀴즈 없으면 최초접속으로 판단(일단은)

--- src/main/java/com/example/A/chime/diary/controller/StartController.java
+++ src/main/java/com/example/A/chime/diary/controller/StartController.java
@@ -1,17 +1,26 @@
 package com.example.A.chime.diary.controller;
 
+import com.example.A.chime.diary.domain.Member;
+import com.example.A.chime.diary.repository.ScoreRepository;
+import com.example.A.chime.diary.service.StartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class StartController {
+    private final StartService startService;
+
     @GetMapping("start")
     public ResponseEntity start(){
-        return ResponseEntity.ok().body(true);
+        Member member = new Member();
+        boolean result = startService.start(member);
+        return ResponseEntity.ok().body(result);
     }
 }
