741c18608bccf62d0ad5123e4d9dc751e90e32be
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-12 01:39:33 +0900

    feat: 퀴즈 가져오는 기능

--- src/main/java/com/example/A/chime/diary/controller/QuizController.java
+++ src/main/java/com/example/A/chime/diary/controller/QuizController.java
@@ -2,13 +2,12 @@ package com.example.A.chime.diary.controller;
 
 import com.example.A.chime.diary.domain.Member;
 import com.example.A.chime.diary.dto.responseDto.QuizChoiceResponse;
-import com.example.A.chime.diary.service.DiaryService;
 import com.example.A.chime.diary.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("quiz")
@@ -16,18 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
     private final QuizService quizService;
     //1. 퀴즈한개씩 가져오기
-    @GetMapping()
-    public ResponseEntity get_quiz(){
+    @GetMapping("/{num}")
+    public ResponseEntity get_quiz(@PathVariable("number") int number){
+
         Member member = new Member();
-        QuizChoiceResponse response = quizService.generate_quizChoice(member);
 
+        // 1--> 날씨 묻는 퀴즈, 3,5--> 일기기반 2,4,6--> 저장된 퀴즈
+        if(number==3 || number==5){
+            QuizChoiceResponse response = quizService.generate_quizChoice(member);
+            return ResponseEntity.ok().body(response);
+        }
+        else if (number==2 || number==4 || number==6) {
+            Long randomValue = getRandomNumberInRange(1, 7);
+            QuizChoiceResponse response = quizService.get_quize(randomValue);
+            return ResponseEntity.ok().body(response);
+        }
+        else if (number==1) {
+            //
+        }
 
-
-
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(null);
     }
 
-    //2. 퀴즈답안 제출했을때
+
+    private Long getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Max must be greater than min");
+        }
+        Random random = new Random();
+        return (long) (random.nextInt((max - min) + 1) + min);
+    }
+
+
+
 
     //3. 퀴즈결과 반환
 }
