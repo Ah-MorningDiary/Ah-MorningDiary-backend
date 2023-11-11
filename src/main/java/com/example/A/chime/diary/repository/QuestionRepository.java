a48950028a93f9ab07750473552711ce608814a3
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-12 01:12:40 +0900

    feat: 퀴즈 결과 저장

--- src/main/java/com/example/A/chime/diary/repository/QuestionRepository.java
+++ src/main/java/com/example/A/chime/diary/repository/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.example.A.chime.diary.repository;
 
-public interface QuestionRepository {
+import com.example.A.chime.diary.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question,Long> {
 }
